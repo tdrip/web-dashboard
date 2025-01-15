2efbe796cef7d6e268f17b22dba8a37f3167f724
tdrip <willbobaggins0@gmail.com>
2025-01-15 20:58:43 +0000

    added specific renders

--- pkg/v1/handlers/renders.go
+++ pkg/v1/handlers/renders.go
@@ -1,6 +1,10 @@
 package handlers
 
-import "github.com/maddalax/htmgo/framework/h"
+import (
+	"github.com/maddalax/htmgo/framework/h"
+	"github.com/tdrip/web-dashboard/pkg/v1/modals"
+	"github.com/tdrip/web-dashboard/pkg/v1/tables"
+)
 
 type GetRender func(c *h.RequestContext) *h.Partial
 type GetFullRender func(c *h.RequestContext) *h.Page
@@ -13,6 +17,20 @@ type PartialRenderHandler struct {
 type PageRenderHandler struct {
 	Handler
 	Renderer GetFullRender
+}
+
+func NewModalRenderHandler(method string, uri string, mr modals.ModalRender) PartialRenderHandler {
+	partial := func(ctx *h.RequestContext) *h.Partial {
+		return modals.RenderModal(mr)
+	}
+	return PartialRenderHandler{Handler: Handler{HttpMethod: method, Uri: uri}, Renderer: partial}
+}
+
+func NewTableRenderHandler(method string, uri string, tr tables.TableRender) PartialRenderHandler {
+	partial := func(ctx *h.RequestContext) *h.Partial {
+		return tables.RenderTable(tr)
+	}
+	return PartialRenderHandler{Handler: Handler{HttpMethod: method, Uri: uri}, Renderer: partial}
 }
 
 func NewPartialRenderHandler(method string, uri string, exec GetRender) PartialRenderHandler {
